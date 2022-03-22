3eab066e43d09b5a6d0fd37a7d072a0f157f90fd
Linn <98117067+LinnBlok@users.noreply.github.com>
2022-03-23 00:06:14 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+/*Сложение*/
+function sumfunction(x, y){
+    const result = x + y;
+    console.log(result);
+}
+sumfunction (4,6);
+
+/*Вычитание*/
+const subfunction = function (c, d){
+    const result = c - d;
+    console.log(result);
+}
+subfunction(10, 5);
+
+
+/*Умножение*/
+const multifunction = (a , b) => {
+    const result = a * b;
+    console.log(result);
+}
+multifunction(2, 2);
+
+
+/*Деление*/
+const divfunction = (f, g) => {
+    const result = f / g;
+    console.log(result);
+}
+divfunction(36, 6);
+
+/*Взятие остатка от деления*/
+function remainsfunction(q, w){
+    const result = q % w;
+    console.log(result);
+}
+remainsfunction(1254, 5);
